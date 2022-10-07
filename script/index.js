0943ce97fd78ce2e73efc846435169b2a3c1d714
Marcos <marcossandim21@gmail.com>
2022-10-07 19:52:02 -0300

    versao 01

---
+++ script/index.js
@@ -0,0 +1,70 @@
+let mobileToggle = document.querySelector('.navbar-toggler');
+let navbarCollapse = document.querySelector('#navbar-links');
+let mobileItens = document.querySelectorAll('.nav-link');
+
+mobileItens.forEach(item => {
+    item.addEventListener('click', event => {
+        mobileToggle.classList.remove('collapsed');
+        navbarCollapse.classList.remove('show');
+    })
+});
+
+
+// Testimonials carrousel
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
+
+});
+
+
+function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+//Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+mybutton.addEventListener('click', topFunction);
